e5e2b3973ac44d400db337a6e9985bfb6cd32eea
trongnhandev1011 <nhantrongho2002@gmail.com>
2023-02-07 00:54:59 +0700

    feat: useRef for optimize and add select countryCode

--- src/containers/FormContainer/Form.container.jsx
+++ src/containers/FormContainer/Form.container.jsx
@@ -1,31 +1,92 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 import FormComponent from "../../components/FormComponent/Form.component";
 import "./Form.style.scss";
+import PhoneInputContainer from "../PhoneInputContainer/PhoneInput.container";
+import CodeInputContainer from "../CodeInputContainer/CodeInput.container";
+
+const phoneRegex =
+  /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+const codeRegex = /\d{6}/;
 
 const FormContainer = () => {
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [errorMsg, setErrorMsg] = useState("");
+  const countryCode = useRef();
+  const phoneNumberRef = useRef();
+  const codeRef = useRef();
+  const [errorMsg, setErrorMsg] = useState({});
 
-  const phoneRegex =
-    /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-
-  useEffect(() => {}, [phoneNumber]);
-
-  const validateForm = (value) => {
-    setErrorMsg("");
+  const validatePhoneNumber = (value) => {
+    setErrorMsg({
+      ...errorMsg,
+      phoneError: "",
+    });
     if (!phoneRegex.test(value)) {
-      setErrorMsg("Please enter a valid phone number");
+      setErrorMsg({
+        ...errorMsg,
+        phoneError: "Phone number is invalid",
+      });
     }
   };
 
+  const validateCodeNumber = (value) => {
+    setErrorMsg({
+      ...errorMsg,
+      codeError: "",
+    });
+    if (!codeRegex.test(value) && value !== "") {
+      setErrorMsg({
+        ...errorMsg,
+        codeError: "Invalid token",
+      });
+    }
+  };
+
+  const callAPI = (api, data) => {
+    fetch(`http://localhost:4000/${api}`, {
+      // Enter your IP address here
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+  };
+
+  const submitForm = () => {
+    let submitData = {};
+
+    if (phoneNumberRef.current.value) {
+      if (!codeRef.current.value) {
+        submitData = {
+          phoneNumber: countryCode.current.value + phoneNumberRef.current.value,
+        };
+        callAPI("createNewAccessCode", submitData);
+      } else {
+        submitData = {
+          phoneNumber: countryCode.current.value + phoneNumberRef.current.value,
+          codeRef: codeRef.current.value,
+        };
+        callAPI("validateAccessCode", submitData);
+      }
+    }
+  };
   return (
     <div className="form-container flex items-center justify-center">
-      <FormComponent
-        phoneNumber={phoneNumber}
-        setPhoneNumber={setPhoneNumber}
-        validateForm={validateForm}
-        errorMsg={errorMsg}
-      />
+      <FormComponent submitForm={submitForm} errorMsg={errorMsg}>
+        <PhoneInputContainer
+          countryCode={countryCode}
+          phoneNumberRef={phoneNumberRef}
+          validatePhoneNumber={validatePhoneNumber}
+          errorMsg={errorMsg.phoneError}
+        />
+        <CodeInputContainer
+          codeRef={codeRef}
+          validateCodeNumber={validateCodeNumber}
+          errorMsg={errorMsg.codeError}
+        />
+      </FormComponent>
     </div>
   );
 };
